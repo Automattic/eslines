9d932fd8d8b0c8810bea9218d6dd7029766d4d2a
Andrés Maneiro <andres.maneiro@automattic.com>
2017-08-11 23:57:43 +0200

    Pass the remote as a parameter
    so we don't use the config inside a lib utility.

--- src/lib/git-diff-calculator.js
+++ src/lib/git-diff-calculator.js
@@ -2,20 +2,17 @@
 
 'use strict';
 
-const fs = require( 'fs' );
 const gitDiffRemoteVSHead = require( '../lib/git-diff' );
 const gitDiffIndex = require( '../lib/git-diff-index' );
 
-const config = JSON.parse( fs.readFileSync( '.eslines.json', 'utf-8' ) );
-
-module.exports = function() {
+module.exports = function( remote ) {
 	const whatToDiff = process.env.ESLINES_DIFF;
 
 	let diff;
 	if ( whatToDiff === 'index' ) {
 		diff = gitDiffIndex();
 	} else { // 'remote'
-		diff = gitDiffRemoteVSHead( config.remote );
+		diff = gitDiffRemoteVSHead( remote );
 	}
 
 	return diff;
--- src/processors/lines-modified.js
+++ src/processors/lines-modified.js
@@ -6,6 +6,6 @@ const gitDiffCalculator = require( '../lib/git-diff-calculator' );
 const config = JSON.parse( fs.readFileSync( '.eslines.json', 'utf-8' ) );
 
 module.exports = function( report ) {
-	const lines = differ( gitDiffCalculator() );
+	const lines = differ( gitDiffCalculator( config.remote ) );
 	return JSON.stringify( eslines( report, lines, config.rulesNotToDowngrade ) );
 };
