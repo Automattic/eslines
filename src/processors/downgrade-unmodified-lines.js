0f2c44e2cea0b29e38e5c03f897904645f708c50
Andrés <nosolosw@users.noreply.github.com>
2017-08-25 17:13:57 +0200

    Better defaults if no config keys (#12)

--- src/processors/downgrade-unmodified-lines.js
+++ src/processors/downgrade-unmodified-lines.js
@@ -6,10 +6,11 @@ const gitDiffCalculator = require( '../lib/git-diff-calculator' );
 const config = JSON.parse( fs.readFileSync( '.eslines.json', 'utf-8' ) );
 
 module.exports = function( report ) {
-	const remote = config.processors[ 'downgrade-unmodified-lines' ].remote || 'origin/master';
+	const processorConfig = config.processors[ 'downgrade-unmodified-lines' ] || {};
+	const remote = processorConfig.remote || 'origin/master';
 	const whatToDiff = process.env.ESLINES_DIFF;
 	const lines = differ( gitDiffCalculator( remote, whatToDiff ) );
 
-	const rulesNotToDowngrade = config.processors[ 'downgrade-unmodified-lines' ].rulesNotToDowngrade || []; // eslint-disable-line max-len
+	const rulesNotToDowngrade = processorConfig.rulesNotToDowngrade || [];
 	return JSON.stringify( downgradeUnmodifiedLines( report, lines, rulesNotToDowngrade ) );
 };
